850d2d8dc213c96bc86d2a91349de21a727dc750
OsosKP <ososkp@gmail.com>
2020-03-04 21:32:45 +0000

    Improve Login functionality robustness Restructure code to better handle logging in, logging out and permissions.

--- src/main/java/com/coolGroup/org/repositories/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package com.coolGroup.org.repositories;
-
-import com.coolGroup.org.models.Login;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login, Integer> {
-
-}
--- src/main/java/com/coolGroup/org/services/concretes/LoginService.java
+++ src/main/java/com/coolGroup/org/services/concretes/LoginService.java
@@ -1,9 +1,8 @@
 package com.coolGroup.org.services.concretes;
+
 import com.coolGroup.org.models.Login;
 import com.coolGroup.org.models.Staff;
 import com.coolGroup.org.models.Student;
-import com.coolGroup.org.repositories.LoginRepository;
-import com.coolGroup.org.repositories.StudentRepository;
 import com.coolGroup.org.services.abstracts.ILoginService;
 import com.coolGroup.org.services.abstracts.IStaffService;
 import com.coolGroup.org.services.abstracts.IStudentService;
@@ -14,53 +13,48 @@ import org.springframework.stereotype.Service;
 public class LoginService implements ILoginService {
     private IStudentService studentService;
     private IStaffService staffService;
-    private StudentRepository studentRepository;
-    private LoginRepository loginRepository;
-    private Login login  = new Login();
+    private Login login;
 
     @Autowired
-    public LoginService(LoginRepository loginRepository , IStudentService studentService) {
-        this.loginRepository = loginRepository;
+    public LoginService(IStudentService studentService, IStaffService staffService) {
         this.studentService = studentService;
-        //this.staffService = staffService;
+        this.staffService = staffService;
+        login = null;
     }
 
     @Override
-    public Login get() {
+    public Login getLoggedInUser() {
         return login;
     }
 
-
     @Override
-    public Login user(Login user) {
-        // If null is returned, the login is incorrect
-
-        if (!user.getEmail().equals("")) {
-            if (user.getUserRole().equals("staff")) {
-                Staff staff = this.staffService.getByEmail(user.getEmail());
-                if (staff.getEmail().equals(user.getEmail()) && user.getPassword().equals(staff.getPassword())) {
-
-                    login = new Login(staff.getId(), staff.getEmail(), user.getUserRole());
-                    // login = this.loginRepository.saveAndFlush(login);
-                } else {
-
-                    login = null;
-                }
-            }
-
-            if (user.getUserRole().equals("student")) {
-                Student student = this.studentService.getByEmail(user.getEmail());
-                if (student.getEmail().equals(user.getEmail()) && user.getPassword().equals(student.getPassword())) {
-
-                    login = new Login(student.getId(), student.getEmail(), user.getUserRole());
-                    // login = this.loginRepository.saveAndFlush(login);
-                } else {
-                    login = null;
-                }
+    public Login loginUser(Login loggedIn) {
+        login = null;
+        loggedIn.setUser_role(loggedIn.getUser_role().toLowerCase());
+        if (loggedIn.getUser_role().equals("staff")) {
+            Staff staff = this.staffService.getByEmail(loggedIn.getEmail());
+            if (staff.getEmail().equals(loggedIn.getEmail()) && loggedIn.getPassword().equals(staff.getPassword())) {
+                login = new Login(staff.getEmail(), "***", loggedIn.getUser_role());
             }
         }
 
+        if (loggedIn.getUser_role().equals("student")) {
+            Student student = this.studentService.getByEmail(loggedIn.getEmail());
+            if (student.getEmail().equals(loggedIn.getEmail()) && loggedIn.getPassword().equals(student.getPassword())) {
+                login = new Login(student.getEmail(), "***", loggedIn.getUser_role());
+            }
+        }
         return login;
+    }
+
+    @Override
+    public void logoutUser() {
+        this.login = null;
+    }
+
+    @Override
+    public String getRole() {
+        return login != null ? login.getUser_role() : "";
     }
 
 }
