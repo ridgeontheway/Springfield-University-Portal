c766e120ff4eef8c9fed21e42030ebbb63a36b9e
sean <sean.letmon@ucdconnect.ie>
2020-05-15 11:41:20 +0100

    revert https

--- src/main/java/com/coolGroup/org/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/coolGroup/org/ReactAndSpringDataRestApplication.java
@@ -3,13 +3,7 @@ package com.coolGroup.org;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ReactAndSpringDataRestApplication {
@@ -18,38 +12,5 @@ public class ReactAndSpringDataRestApplication {
 		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
 	}
 
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		// Enable SSL Trafic
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
 
-		// Add HTTP to HTTPS redirect
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-		return tomcat;
-	}
-
-	/*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8080. With SSL it will use port 8443. So, any request for 8080 needs to be
-    redirected to HTTPS on 8443.
-     */
-	private Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
-	}
 }
